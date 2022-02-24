7e58566a5dd083acfec47a2a644eb0663c9ab045
kyo212 <kyohy163@gmail.com>
2022-02-24 17:16:32 +0900

    検索バーのUIを変更

--- client/src/components/molecles/tabs/FooterTab.jsx
+++ client/src/components/molecles/tabs/FooterTab.jsx
@@ -12,11 +12,11 @@ export const FooterTab = () => {
     <div className="fixed bottom-0 h-14 w-full border-t-2 bg-white">
       <div className="flex h-full w-full items-center space-x-2">
         <p>
-          <a href="#" className="border-b mx-8 text-[12px] text-blue-700">
+          <a href="#" className="mx-8 border-b text-[12px] text-blue-700">
             みんなの思い出
           </a>
         </p>
-        <button className="fixed right-2 flex items-center rounded-sm border-slate-400 bg-white">
+        <button className="fixed border-2 right-2 flex items-center rounded-md border-slate-300 bg-white">
           <input
             type="text"
             className={[seachToggle ? searchOpen.showed : searchOpen.base]}
