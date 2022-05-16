0db8fb2c9d3b1031edb60bf38f7eab54cffb5f34
kyo212 <kyohy163@gmail.com>
2022-05-16 14:50:16 +0900

    aタグをuseNavigateに変更

--- client/src/root/NotFound.jsx
+++ client/src/root/NotFound.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="h-screen w-screen flex flex-col items-center pt-20 space-y-2 bg-slate-100">
+    <div className="flex h-screen w-screen flex-col items-center space-y-2 bg-slate-100 pt-20">
       <p className="text-4xl font-bold">404 Not found</p>
       <p>ページが見つかりません</p>
-      <button className="my-2">
-        <a href="/" className="text-blue-800 border-b border-blue-800">ホームへ戻る</a>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+        className="my-2 border-b border-blue-800 text-blue-800"
+      >
+        ホームへ戻る
       </button>
     </div>
   );
