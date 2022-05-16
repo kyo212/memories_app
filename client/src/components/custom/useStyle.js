bfe75a1a09481be9aafe7bbc4cfa882e9a410991
kyo212 <kyohy163@gmail.com>
2022-05-16 11:25:07 +0900

    プレースホルダーのテキストサイズをsmに変更

--- client/src/components/custom/useStyle.js
+++ client/src/components/custom/useStyle.js
@@ -57,7 +57,7 @@ export const useStyle = () => {
         "w-[90%] z-50 border border-green-500 rounded-lg fixed top-14 left-1/2 -translate-x-1/2 bg-green-600 bg-green-100 px-4 py-2 text-sm text-green-800 opacity-100 transition-all duration-500",
     },
     errorBorderMsg: {
-      base: "w-full rounded border border-slate-300 px-4 py-2",
+      base: "w-full rounded border border-slate-300 px-4 py-2 placeholder:text-sm",
       showed: "w-full rounded border px-4 border border-red-400 py-2",
     },
   };
